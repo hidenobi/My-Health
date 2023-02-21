455025bf04bb7b1edfc13cd2e973e43b2b8cb204
TuanVH <dungtuan095@gmail.com>
2023-02-21 21:06:04 +0700

    Setup navigation + fragment

--- app/src/main/java/com/hidenobi/myhealth/MainActivity.kt
+++ app/src/main/java/com/hidenobi/myhealth/MainActivity.kt
@@ -2,10 +2,29 @@ package com.hidenobi.myhealth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.hidenobi.myhealth.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var hostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpNavController()
+        setUpBottomNavigation()
+    }
+    private fun setUpBottomNavigation() {
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+
+    private fun setUpNavController() {
+        hostFragment =
+            supportFragmentManager.findFragmentById(R.id.frameContainer) as NavHostFragment
+        navController = hostFragment.navController
     }
 }
