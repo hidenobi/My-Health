a92206a04bf11643fd59bcb2fcb163e7c5c1a8ab
TuanVH <dungtuan095@gmail.com>
2023-03-06 11:51:41 +0700

    create home fragment UI

--- app/src/main/java/com/hidenobi/myhealth/homefragment/HomeFragment.kt
+++ app/src/main/java/com/hidenobi/myhealth/homefragment/HomeFragment.kt
@@ -1,19 +1,21 @@
 package com.hidenobi.myhealth.homefragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.hidenobi.myhealth.R
+import androidx.fragment.app.Fragment
+import com.hidenobi.myhealth.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
 
